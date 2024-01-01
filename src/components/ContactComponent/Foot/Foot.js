ea1040927fb724b61b0e9eccbd69ea84edd6d5a6
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-01-01 18:24:30 +0100

    contact

---
+++ src/components/ContactComponent/Foot/Foot.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Foot() {
+  return (
+    <div className='container'>
+        <ul className='f-nav'>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Contact uS</a></li>
+            <li><a href='#'>Testimonials</a></li>
+        </ul>
+
+        <ul className='s-nav'>
+            <li><a href='#'>About Us</a></li>
+            <li><a href='#'>Our internship</a></li>
+            <li><a href='#'>Newsletter</a></li>
+        </ul>
+
+        <div className='contact'>
+            <div className='icon'>
+
+            </div>
+            <p>@productsquare</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Foot
--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainNav from '../Layout/MainNav/MainNav'
 import Body from '../components/ContactComponent/Body/Body'
+import Foot from '../components/ContactComponent/Foot/Foot'
 
 function ContactPage() {
   return (
     <div>
         <MainNav/>
         <Body />
+        <Foot />
     </div>
    
   )
