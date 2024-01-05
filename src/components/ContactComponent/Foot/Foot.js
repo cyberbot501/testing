460e11d8b8ff12723c114a5da6dcca0145d62dbb
cyberbot501 <softwareengineerjoycy@gmail.com>
2024-01-05 23:25:49 +0100

    footetr

--- src/components/ContactComponent/Foot/Foot.js
+++ src/components/ContactComponent/Foot/Foot.js
@@ -3,35 +3,52 @@ import env from "../../../Assets/envelope.png"
 import lin from "../../../Assets/linkdn.png"
 import twi from "../../../Assets/twitter.png"
 import wha from "../../../Assets/whatsapp.png"
+import loga from "../../../Assets/form-img.png"
 
 import './foot.css'
 function Foot() {
   return (
-    <div className='containers'>
-        <ul className='f-nav'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Contact uS</a></li>
-            <li><a href='#'>Testimonials</a></li>
-        </ul>
+    <div className='count'>
 
-        <ul className='s-nav'>
-            <li><a href='#'>About Us</a></li>
-            <li><a href='#'>Our internship</a></li>
-            <li><a href='#'>Newsletter</a></li>
-        </ul>
+        <div className='-e-box'>
+            
+        </div>
 
-        <div className='contact'>
-            <div className='icon'>
-                <img src={env} alt=''/>
-                <img src={twi} alt=''/>
-                <img src={lin} alt=''/>
-                <img src={wha} alt=''/>
+        <div className='containers'>
+            
+            <ul className='f-nav'>
+                <li> <img src={loga} alt=''/></li>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Contact uS</a></li>
+                <li><a href='#'>Testimonials</a></li>
+            </ul>
+
+            <ul className='s-nav'>
+                <li><a href='#'>About Us</a></li>
+                <li><a href='#'>Our internship</a></li>
+                <li><a href='#'>Newsletter</a></li>
+            </ul>
+
+            <div className='contact'>
+                <div className='icon'>
+                    <img src={env} alt=''/>
+                    <img src={twi} alt=''/>
+                    <img src={lin} alt=''/>
+                    <img src={wha} alt=''/>
+                </div>
+                <p>@productsquare</p>
             </div>
-            <p>@productsquare</p>
+
+        </div>
+
+        <div class="custom-shape-divider-top-1704490432">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+            </svg>
         </div>
 
     </div>
-  )
+     )
 }
 
 export default Foot
